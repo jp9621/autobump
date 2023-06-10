30ef1b95d5787e509d76cc431add3f52320bd1b6
jp9621 <107367682+jp9621@users.noreply.github.com>
2023-06-09 21:23:26 -0500

    Firefox Update
    Changed the browser to firefox

--- autobump.py
+++ autobump.py
@@ -1,21 +1,20 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 
 def automate_browser():
-    chrome_options = Options()
-    #chrome_options.add_argument("--headless")  # Run Chrome in headless mode (without opening a window)
-    driver = webdriver.Chrome(options=chrome_options)
+    firefox_options = Options()
+    #firefox_options.add_argument("--headless")  # Run Firefox in headless mode (without opening a window)
+    driver = webdriver.Firefox(options=firefox_options)
     
-    # Replace "https://example.com" with the desired link
     driver.get("https://disboard.org/server/bump/1063705551625797683")
     
     # Wait for the page to load (adjust the sleep time if needed)
     time.sleep(15)
     
     # Close the browser window
-    #driver.quit()
+    # driver.quit()
 
 while True:
     automate_browser()
